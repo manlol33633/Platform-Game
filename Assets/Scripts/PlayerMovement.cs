9c49ed76b115e63b9b8dc315c65e64f3374caa27
manlol33633 <51886476+manlol33633@users.noreply.github.com>
2022-11-07 08:05:34 -0600

    GFUIgsauefhoisz

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    private bool isGrounded;
+    private Rigidbody2D rb2d;
+    private int jumpCount;
+    private float horizontalMovement;
+    
+    void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
+        jumpCount = 0;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        horizontalMovement = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate() {
+        rb2d.velocity = new Vector2(horizontalMovement * speed, rb2d.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < 3) {
+            rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            isGrounded = false;
+            jumpCount++;
+        } else if (isGrounded) {
+            jumpCount = 0;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Ground") {
+            isGrounded = true;
+        }
     }
 }
