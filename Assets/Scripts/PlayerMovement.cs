0e301b5b85676f461ce83bbfd4d16f63de558c32
manlol33633 <51886476+manlol33633@users.noreply.github.com>
2022-11-09 08:00:59 -0600

    aw;djawjiohd

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
+    private int wallJumpCount;
     private bool isGrounded;
+    private bool isWallSliding;
     private Rigidbody2D rb2d;
     private int jumpCount;
     private float horizontalMovement;
@@ -14,6 +16,7 @@ public class PlayerMovement : MonoBehaviour
     void Start() {
         rb2d = GetComponent<Rigidbody2D>();
         jumpCount = 0;
+        wallJumpCount = 0;
     }
 
     void Update() {
@@ -21,11 +24,18 @@ public class PlayerMovement : MonoBehaviour
 
         rb2d.velocity = new Vector2(horizontalMovement * speed, rb2d.velocity.y);
         if (Input.GetKeyDown(KeyCode.Space) && jumpCount < 3) {
-            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+            //rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+            rb2d.velocity = new Vector2(jumpForce, jumpForce);
             isGrounded = false;
             jumpCount++;
         } else if (isGrounded) {
             jumpCount = 0;
+        } else if (isWallSliding && Input.GetKeyDown(KeyCode.Space)) {
+            rb2d.gravityScale = 1;
+            rb2d.velocity = jumpForce * transform.up;
+            rb2d.velocity = jumpForce * transform.right;
+        } else {
+            rb2d.gravityScale = 2;
         }
     }
 
@@ -36,6 +46,18 @@ public class PlayerMovement : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Ground") {
             isGrounded = true;
+        }
+        if (collision.gameObject.tag == "Wall") {
+            isWallSliding = true; 
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other) {
+        if (other.gameObject.tag == "Ground") {
+            isGrounded = false;
+        }
+        if (other.gameObject.tag == "Wall") {
+            isWallSliding = false; 
         }
     }
 }
